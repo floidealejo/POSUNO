efc9f118eb6e7cbb2bc4df80b9c9014959c58c6c
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-06-15 20:50:56 -0500

    [ADD] Initial DB

---
+++ POSUNO/POSUNO.Api/Data/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Data.Entities
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }     
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [MinLength(6)]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
