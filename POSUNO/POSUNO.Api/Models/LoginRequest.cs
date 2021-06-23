330a7d5afcd5af754171ebbcc57e8b48fb2ddb2e
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-06-22 20:24:28 -0500

    [ADD] First controller for Login and  Products

---
+++ POSUNO/POSUNO.Api/Models/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
